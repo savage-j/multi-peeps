2b4b6eb507c586b651e2139bd8321e78a2f500dd
Jordan <savage@savagej.com>
2016-07-24 13:40:20 -0700

    create views w/mock data

---
+++ multi-peers/multi-peers/ConversationViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ConversationViewCell.swift
+//  multi-peers
+//
+//  Created by Jordan Carlson on 7/24/16.
+//  Copyright © 2016 savagej. All rights reserved.
+//
+
+import UIKit
+
+class ConversationViewCell: UITableViewCell {
+
+    @IBOutlet var avatar: UIImageView!
+
+    @IBOutlet var lastMessage: UILabel!
+
+    @IBOutlet var peerName: UILabel!
+}
--- multi-peers/multi-peers/ViewController.swift
+++ multi-peers/multi-peers/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func login(sender: AnyObject) {
+        
+        self.performSegueWithIdentifier("login", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
